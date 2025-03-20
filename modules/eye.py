3bf27f43bdc054d70aed3067e67d7511b3a50ca0
Harry Boyd <hboyd255@gmail.com>
2025-03-20 20:54:46 +0000

    Added windows support to Eye class

--- modules/eye.py
+++ modules/eye.py
@@ -1,50 +1,92 @@
+import platform
 import cv2
-import picamera2
 
 
-class Eye:
+if platform == "Linux":
 
-    def __init__(self, side="left"):
+    import picamera2  # type: ignore
 
-        side = side.casefold()
+    class Eye:
 
-        if side == "left" or side == "l":
-            index = 0
-        elif side == "right" or side == "r":
-            index = 1
-        else:
-            # TODO Add error case.
-            return
+        def __init__(self, side="left"):
 
-        self.cam = picamera2.Picamera2(camera_num=index)
+            side = side.casefold()
 
-        # For wahtever reason, in this parameter RGB and BGR are flipped. This
-        # module actually returns a BGR image.
-        config = self.cam.create_video_configuration(
-            main={"size": (2304, 1296), "format": "RGB888"}
-        )
-        # TODO do tests into best base resolution
+            if side == "left" or side == "l":
+                index = 0
+            elif side == "right" or side == "r":
+                index = 1
+            else:
+                # TODO Add error case.
+                return
 
-        self.cam.configure(config)
-        self.cam.start()
+            self.cam = picamera2.Picamera2(camera_num=index)
 
-    def array(self, format="bgr"):
-        full_res = self.cam.capture_array()
+            # For whatever reason, in this parameter RGB and BGR are flipped.
+            # This module actually returns a BGR image.
+            config = self.cam.create_video_configuration(
+                main={"size": (2304, 1296), "format": "RGB888"}
+            )
+            # TODO do tests into best base resolution
 
-        # TODO do tests into best scaled resolution.
-        scaled_down = full_res[::4, ::4]
+            self.cam.configure(config)
+            self.cam.start()
 
-        if format.casefold() == "rgb":
-            scaled_down = cv2.cvtColor(scaled_down, cv2.COLOR_BGR2RGB)
+        def array(self, format="bgr"):
+            full_res = self.cam.capture_array()
 
-        return scaled_down
+            # TODO do tests into best scaled resolution.
+            scaled_down = full_res[::4, ::4]
 
-    def jpeg(self, quality=50):
+            if format.casefold() == "rgb":
+                scaled_down = cv2.cvtColor(scaled_down, cv2.COLOR_BGR2RGB)
 
-        frame = self.array()
+            return scaled_down
 
-        params = [cv2.IMWRITE_JPEG_QUALITY, quality]
+        def jpeg(self, quality=50):
 
-        jpeg = cv2.imencode(".jpg", frame, params)[1].tobytes()
+            frame = self.array()
 
-        return jpeg
+            params = [cv2.IMWRITE_JPEG_QUALITY, quality]
+
+            jpeg = cv2.imencode(".jpg", frame, params)[1].tobytes()
+
+            return jpeg
+
+elif platform.system() == "Windows":
+
+    class Eye:
+
+        def __init__(self, side="left"):
+
+            side = side.casefold()
+
+            if side == "left" or side == "l":
+                index = 0
+            elif side == "right" or side == "r":
+                index = 1
+            else:
+                # TODO Add error case.
+                return
+
+            self.cam = cv2.VideoCapture(index)
+
+        def array(self, format="bgr"):
+            _, full_res = self.cam.read()
+
+            if format.casefold() == "rgb":
+                scaled_down = cv2.cvtColor(scaled_down, cv2.COLOR_BGR2RGB)
+
+            return scaled_down
+
+        def jpeg(self, quality=50):
+
+            frame = self.array()
+
+            params = [cv2.IMWRITE_JPEG_QUALITY, quality]
+
+            jpeg = cv2.imencode(".jpg", frame, params)[1].tobytes()
+
+            return jpeg
+
+        
