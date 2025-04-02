e63e6c3a5dec98c391dc1b1798c6c12e3980029f
Harry Boyd <hboyd255@gmail.com>
2025-04-02 12:53:54 +0100

    Added modes to stream video

--- stream_video.py
+++ stream_video.py
@@ -1,22 +1,85 @@
+import cv2
 from flask import Flask, Response
-from modules.eye import Eye
+from modules.vision import Vision
 
 app = Flask(__name__)
-cam = Eye("Left")
+eyes = Vision()
 
 
-def generate():
-    while True:
+def generate(mode):
 
-        jpeg = cam.jpeg()
+    if mode.casefold() == "single":
+        while True:
 
-        yield b"--frame\r\nContent-Type: image/jpeg\r\n\r\n" + jpeg + b"\r\n"
+            jpeg = eyes.left.jpeg()
+
+            yield b"--frame\r\nContent-Type: image/jpeg\r\n\r\n" + jpeg + b"\r\n"
+
+    if mode.casefold() == "both":
+        while True:
+
+            frame = eyes.joined_array()
+            params = [cv2.IMWRITE_JPEG_QUALITY, 50]
+            jpeg = cv2.imencode(".jpg", frame, params)[1].tobytes()
+
+            yield b"--frame\r\nContent-Type: image/jpeg\r\n\r\n" + jpeg + b"\r\n"
+
+    if mode.casefold() == "onion":
+        while True:
+
+            frame = eyes.onion_array()
+            params = [cv2.IMWRITE_JPEG_QUALITY, 50]
+            jpeg = cv2.imencode(".jpg", frame, params)[1].tobytes()
+
+            yield b"--frame\r\nContent-Type: image/jpeg\r\n\r\n" + jpeg + b"\r\n"
+
+    if mode.casefold() == "redblue":
+        while True:
+
+            frame = eyes.anaglyph_array()
+            params = [cv2.IMWRITE_JPEG_QUALITY, 90]
+            jpeg = cv2.imencode(".jpg", frame, params)[1].tobytes()
+
+            yield b"--frame\r\nContent-Type: image/jpeg\r\n\r\n" + jpeg + b"\r\n"
 
 
 @app.route("/video")
 def video():
     return Response(
-        generate(),
+        generate("single"),
+        mimetype="multipart/x-mixed-replace; boundary=frame",
+        headers={
+            "Cache-Control": "max-age=0, no-store, no-cache, must-revalidate"
+        },
+    )
+
+
+@app.route("/both")
+def both():
+    return Response(
+        generate("both"),
+        mimetype="multipart/x-mixed-replace; boundary=frame",
+        headers={
+            "Cache-Control": "max-age=0, no-store, no-cache, must-revalidate"
+        },
+    )
+
+
+@app.route("/onion")
+def onion():
+    return Response(
+        generate("onion"),
+        mimetype="multipart/x-mixed-replace; boundary=frame",
+        headers={
+            "Cache-Control": "max-age=0, no-store, no-cache, must-revalidate"
+        },
+    )
+
+
+@app.route("/redblue")
+def redblue():
+    return Response(
+        generate("redblue"),
         mimetype="multipart/x-mixed-replace; boundary=frame",
         headers={
             "Cache-Control": "max-age=0, no-store, no-cache, must-revalidate"
