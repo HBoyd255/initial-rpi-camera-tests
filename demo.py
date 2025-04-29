ff763108241dc37ac9305a3cbf5232b78d30bc35
Harry Boyd <hboyd255@gmail.com>
2025-04-29 23:52:29 +0100

    Updated stereoDepth.py

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 from collections import deque
 import time
+import cv2
 import numpy
 from modules.colours import *
 from modules.evaluateVariable import evaluate_variable
@@ -44,11 +45,100 @@ localiser = Localiser()
 fps = FPS()
 
 top_down = TopDown(x_plot_bounds=(-0.25, 0.75), draw_grid=False)
+top_full = TopDown(x_plot_bounds=(-0.5, 4.5))
 
 
 history = deque(maxlen=10)
 
 wheel_control = Wheels()
+
+
+def normalize_speeds(speeds: numpy.ndarray) -> numpy.ndarray:
+
+    # Find the maximum absolute value
+    max_value = numpy.max(numpy.abs(speeds))
+
+    if max_value > 100:
+        speeds = speeds / max_value * 100
+
+        speeds = speeds.astype(int)
+
+    return speeds
+
+
+def probabilistic(power):
+
+    sine = -1
+    if power > 0:
+        sine = 1
+
+    trip_power = 80
+
+    rand = numpy.random.randint(0, trip_power)
+
+    if abs(power) > rand:
+        return trip_power * sine
+
+    return 0
+
+
+def match_distance_and_angle(hand: Hand, hand_points):
+    wrist_distance = hand_points[0][1]
+
+    rx = hand.landmarks[0][0]
+
+    vertical_error = (wrist_distance - 1) * 2
+    horizontal_error = (0.5 - rx) * 5
+
+    vertical_error = int(vertical_error * 100)
+    horizontal_error = int(horizontal_error * 100)
+
+    vertical_error = probabilistic(vertical_error)
+    horizontal_error = probabilistic(horizontal_error)
+
+    speeds = numpy.zeros(4, dtype=int)
+
+    speeds += (
+        vertical_error,
+        vertical_error,
+        vertical_error,
+        vertical_error,
+    )
+
+    speeds += (
+        -horizontal_error,
+        horizontal_error,
+        -horizontal_error,
+        horizontal_error,
+    )
+
+    speeds = normalize_speeds(speeds)
+
+    wheel_control.send(*speeds)
+
+
+def match_angle(hand: Hand):
+
+    rx = hand.landmarks[0][0]
+
+    horizontal_error = (0.5 - rx) * 5
+
+    horizontal_error = int(horizontal_error * 100)
+
+    horizontal_error = probabilistic(horizontal_error)
+
+    speeds = numpy.zeros(4, dtype=int)
+
+    speeds += (
+        -horizontal_error,
+        horizontal_error,
+        -horizontal_error,
+        horizontal_error,
+    )
+
+    speeds = normalize_speeds(speeds)
+
+    wheel_control.send(*speeds)
 
 
 def capture_hand(side: str, queue: Queue):
@@ -90,6 +180,7 @@ def draw_square_on_ground(frame, ground_coord):
     point_list.append(point1)
 
     top_down.add_line(point_list, colour=MAGENTA, width=1)
+    top_full.add_line(point_list, colour=MAGENTA, width=1)
 
     return drawing_frame
 
@@ -130,13 +221,19 @@ def main_loop(vid: Video):
 
     gesture_name = gest.get_gesture_name(raw_coords)
 
+    print(gesture_name)
+
     user_is_pointing = gesture_name == "Index"
-    flipped_off = gesture_name == "Middle"
-    is_spider_man = gesture_name == "Spider-Man"
+    double_point = gesture_name == "German Three"
+    # flipped_off = gesture_name == "Middle"
+    # is_spider_man = gesture_name == "Spider-Man"
+    user_holding_fist = gesture_name == "Fist"
+    puppy_dog_mode = gesture_name == "Rock On"
 
     top_down.add_hand_points(hand_coords)
+    top_full.add_hand_points(hand_coords)
 
-    if user_is_pointing:
+    if user_is_pointing or double_point:
 
         tip = hand_coords[8]
 
@@ -167,18 +264,52 @@ def main_loop(vid: Video):
         top_down.add_line(
             [hand_coords[8], ground_point], colour=MAGENTA, width=1
         )
+        top_full.add_line(
+            [hand_coords[8], ground_point], colour=MAGENTA, width=1
+        )
 
         frame_an = localiser.line_3d(
             frame_an, [hand_coords[8], ground_point], colour=(0, 0, 255)
         )
 
-    if flipped_off:
-        print("Thats rude, im leaving")
-        wheel_control.send(-80, -80, -80, -80)
+        if double_point:
 
-    if is_spider_man:
-        print("I love Spider-Man")
-        wheel_control.send(80, 80, 80, 80)
+            az_poi = numpy.arctan2(ground_point[0], ground_point[1])
+
+            angle_error = -numpy.rad2deg(az_poi) * 3
+
+            angle_error = probabilistic(angle_error)
+
+            speeds = numpy.zeros(4, dtype=float)
+
+            speeds += (
+                -angle_error,
+                angle_error,
+                -angle_error,
+                angle_error,
+            )
+
+            speeds = speeds.astype(int)
+
+            print(speeds)
+
+            wheel_control.send(*speeds)
+
+            print()
+
+    #     if flipped_off:
+    #         print("Thats rude, im leaving")
+    #         wheel_control.send(-80, -80, -80, -80)
+    #
+    #     if is_spider_man:
+    #         print("I love Spider-Man")
+    #         wheel_control.send(80, 80, 80, 80)
+
+    if user_holding_fist:
+        match_angle(left_hand)
+
+    if puppy_dog_mode:
+        match_distance_and_angle(left_hand, raw_coords)
 
     vid.show("Projection", frame_an)
 
@@ -186,12 +317,17 @@ def main_loop(vid: Video):
 
     vid.show("Top Down", top_down_image)
 
-    # print(fps)
+    
+    top_down_image_full = top_full.get_image()
+
+    vid.show("Top Down Full", top_down_image_full)
+
+    print(fps)
 
 
 def show():
 
-    vid = Video(canvas_framing=(2, 2))
+    vid = Video(canvas_framing=(3,2))
 
     while True:
 
--- stereoDepth.py
+++ stereoDepth.py
@@ -1,11 +1,12 @@
-from collections import namedtuple
+import time
+from typing import NamedTuple, cast
 import cv2
 import numpy
 import mediapipe
 from modules.eye import Eye
-from queue import Queue
-from threading import Thread
+from multiprocessing import Process, Queue
 
+from modules.hand import Hand
 from modules.localiser import Localiser
 from modules.wheels import Wheels
 from modules.video import Video
@@ -15,18 +16,40 @@ mp_hands = mediapipe.solutions.hands
 mp_drawing = mediapipe.solutions.drawing_utils
 mp_drawing_styles = mediapipe.solutions.drawing_styles
 
-FrameStruct = namedtuple("FrameStruct", ["frame", "hand"])
+
+class FrameStruct(NamedTuple):
+    frame: numpy.ndarray
+    hand: Hand
+
 
 left_queue = Queue(maxsize=1)
 right_queue = Queue(maxsize=1)
 
-vid = Video()
+# Cast for type checking and autocomplete
+left_queue = cast("Queue[FrameStruct]", left_queue)
+right_queue = cast("Queue[FrameStruct]", right_queue)
 
 
 wheel_control = Wheels()
 
 
 localiser = Localiser()
+
+
+def probabilistic(power):
+
+    sine = -1
+    if power > 0:
+        sine = 1
+
+    trip_power = 80
+
+    rand = numpy.random.randint(0, trip_power)
+
+    if abs(power) > rand:
+        return trip_power * sine
+
+    return 0
 
 
 def normalize_speeds(speeds: numpy.ndarray) -> numpy.ndarray:
@@ -49,162 +72,119 @@ def capture_hand(side: str, queue: Queue):
     hand_finder = Zoom()
     while True:
 
+        if queue.full():
+            time.sleep(0.01)
+
         frame = eye.array(res="full")
 
-        hand = hand_finder.get_hand(frame)
+        hand = hand_finder.get_hand(frame, simple=False)
 
-        frame = frame[::4, ::4]
+        frame = numpy.array(frame[::4, ::4])
+
+        frame = hand_finder.draw_zoom_outline(frame)
 
         queue.put(FrameStruct(frame, hand))
 
 
+def main_loop(vid: Video):
+    if left_queue.empty() or right_queue.empty():
+        return
+
+    left_frame, left_hand = left_queue.get()
+    right_frame, right_hand = right_queue.get()
+
+    left_feed = numpy.copy(left_frame)
+    right_feed = numpy.copy(right_frame)
+
+    left_feed = left_hand.draw(left_feed)
+    right_feed = right_hand.draw(right_feed)
+
+    vid.show("Left Feed", left_feed)
+    vid.show("Right Feed", right_feed)
+
+    if not (left_hand.is_seen() and right_hand.is_seen()):
+        return
+
+    distances = localiser.get_distances(left_hand, right_hand)
+
+    wrist_distance = distances[0]
+
+    canv = numpy.zeros_like(left_feed)
+
+    cv2.putText(
+        canv,
+        f"Wrist = {distances[0]:.4}",
+        (30, 100),
+        cv2.FONT_HERSHEY_TRIPLEX,
+        2,
+        (0, 0, 255),
+        2,
+    )
+    cv2.putText(
+        canv,
+        f"Index = {distances[8]:.4}",
+        (30, 200),
+        cv2.FONT_HERSHEY_TRIPLEX,
+        2,
+        (0, 0, 255),
+        2,
+    )
+
+    vid.show("", canv)
+
+    rx = left_hand.landmarks[0][0]
+
+    vertical_error = (wrist_distance - 1) * 2
+    horizontal_error = (0.5 - rx) * 5
+
+    vertical_error = int(vertical_error * 100)
+    horizontal_error = int(horizontal_error * 100)
+
+    vertical_error = probabilistic(vertical_error)
+    horizontal_error = probabilistic(horizontal_error)
+    #
+    #     print(
+    #         f"Wrist = {wrist_distance:.4}, "
+    #         f"vertical_error = {vertical_error} "
+    #         f"horizontal_error = {horizontal_error}"
+    #     )
+
+    speeds = numpy.zeros(4, dtype=int)
+
+    speeds += (
+        vertical_error,
+        vertical_error,
+        vertical_error,
+        vertical_error,
+    )
+
+    speeds += (
+        -horizontal_error,
+        horizontal_error,
+        -horizontal_error,
+        horizontal_error,
+    )
+
+    speeds = normalize_speeds(speeds)
+
+    wheel_control.send(*speeds)
+
+
 def show():
+
+    vid = Video()
 
     while True:
 
-        if not left_queue.empty() and not right_queue.empty():
-
-            left_frame, left_hand = left_queue.get()
-            right_frame, right_hand = right_queue.get()
-
-            left_feed = numpy.copy(left_frame)
-            right_feed = numpy.copy(right_frame)
-
-            if left_hand.is_seen():
-                left_feed = left_hand.draw(left_feed)
-            if right_hand.is_seen():
-                right_feed = right_hand.draw(right_feed)
-
-            vid.show("Left Feed", left_feed)
-            vid.show("Right Feed", right_feed)
-
-            if not (left_hand.is_seen() and right_hand.is_seen()):
-                continue
-
-            distances = localiser.get_distances(left_hand, right_hand)
-
-            vid.show("Disparity", left_feed)
-
-            wrist_distance = distances[0]
-
-            print(
-                f"Wrist = {distances[0]:.4}, Index = {distances[8]:.4}", end=" "
-            )
-
-            canv = numpy.zeros_like(left_feed)
-
-            cv2.putText(
-                canv,
-                f"Wrist = {distances[0]:.4}",
-                (30, 100),
-                cv2.FONT_HERSHEY_TRIPLEX,
-                2,
-                (0, 0, 255),
-                2,
-            )
-            cv2.putText(
-                canv,
-                f"Index = {distances[8]:.4}",
-                (30, 200),
-                cv2.FONT_HERSHEY_TRIPLEX,
-                2,
-                (0, 0, 255),
-                2,
-            )
-
-            vid.show("", canv)
-
-
-#             rx = left_hand.landmarks[0][0]
-#
-#             vertical_error = wrist_distance - 1
-#             vertical_error = vertical_error * 2
-#
-#             MAX_VAL = 100
-#
-#             if vertical_error > MAX_VAL:
-#                 vertical_error = MAX_VAL
-#
-#             if vertical_error < -MAX_VAL:
-#                 vertical_error = -MAX_VAL
-#
-#             horizontal_error = 0.5 - rx
-#
-#             horizontal_error *= 5
-#
-#             if horizontal_error > MAX_VAL:
-#                 horizontal_error = MAX_VAL
-#
-#             if horizontal_error < -MAX_VAL:
-#                 horizontal_error = -MAX_VAL
-#
-#             vertical_error = int(vertical_error * 100)
-#             horizontal_error = int(horizontal_error * 100)
-#
-#             # if abs(horizontal_error) < 100:
-#             r = numpy.random.randint(0, 100)
-#
-#             sine = -1
-#
-#             if horizontal_error > 0:
-#                 sine = 1
-#
-#             print(f"r = {r}, " f"h = {horizontal_error} ", end="")
-#
-#             if abs(horizontal_error) > r:
-#                 horizontal_error = 80 * sine
-#             else:
-#                 horizontal_error = 0
-#
-#             # -------------------
-#
-#             r = numpy.random.randint(0, 100)
-#
-#             sine = -1
-#
-#             if vertical_error > 0:
-#                 sine = 1
-#
-#             print(f"r = {r}, " f"v = {vertical_error} ", end="")
-#
-#             if abs(vertical_error) > r:
-#                 vertical_error = 100 * sine
-#             else:
-#                 vertical_error = 0
-#
-#             print(
-#                 f"Wrist = {wrist_distance:.4}, "
-#                 f"vertical_error = {vertical_error} "
-#                 f"horizontal_error = {horizontal_error}"
-#             )
-#
-#             speeds = numpy.zeros(4, dtype=int)
-#
-#             speeds += [
-#                 vertical_error,
-#                 vertical_error,
-#                 vertical_error,
-#                 vertical_error,
-#             ]
-#
-#             speeds += [
-#                 -horizontal_error,
-#                 horizontal_error,
-#                 -horizontal_error,
-#                 horizontal_error,
-#             ]
-#
-#             speeds = normalize_speeds(speeds)
-#
-#             wheel_control.send(*speeds)
+        main_loop(vid)
 
 
 if __name__ == "__main__":
 
-    left_thread = Thread(target=capture_hand, args=("left", left_queue))
-    right_thread = Thread(target=capture_hand, args=("right", right_queue))
+    left_thread = Process(target=capture_hand, args=("left", left_queue))
+    right_thread = Process(target=capture_hand, args=("right", right_queue))
 
-    display_thread = Thread(target=show)
+    display_thread = Process(target=show)
 
     left_thread.start()
     right_thread.start()
