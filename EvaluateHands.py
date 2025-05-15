7b273afe1966df8eb4d270643f8dd2a1bd44f105
Harry Boyd <hboyd255@gmail.com>
2025-05-15 20:19:51 +0100

    Simplified hand switching in zoom class

--- EvaluateHands.py
+++ EvaluateHands.py
@@ -1,150 +1,156 @@
-from functools import lru_cache
 import time
 import cv2
 
 import mediapipe
 import numpy
-from modules.duration import Duration
 from modules.eye import Eye
-from modules.video import Video
 
-vid = Video(canvas_framing=(1, 1))
-eye = Eye("left")
-
+from modules.zoomLive import Zoom
 
 mp_hands = mediapipe.solutions.hands
+hands = mp_hands.Hands(static_image_mode=False)
 
-hands = mp_hands.Hands(static_image_mode=True)
-
-
-hands_results = []
+baseline_a = []
+baseline_b = []
+total_times = []
 
 
 def get_frame(i):
-    path = f"data/video2/frame_{i}.png"
+    path = f"data/video/frame_{i}.png"
 
     frame = cv2.imread(path)
 
     return frame
 
 
-dura = Duration()
-
-dura.head()
-print("Time to load images from memory and convert to RGB")
+print("Time to load images from memory and convert to RGB again")
 for i in range(100):
 
     print(f"{i}/100")
 
+    start = time.time()
+
     frame = get_frame(i)
+
     bgr = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-dura.flag()
+    end = time.time()
+    duration = end - start
+
+    baseline_a.append(duration)
+
 
 print("Time to load images from memory and convert to RGB again")
 for i in range(100):
 
     print(f"{i}/100")
 
+    start = time.time()
+
     frame = get_frame(i)
+
     bgr = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-dura.flag()
+    end = time.time()
+    duration = end - start
+
+    baseline_b.append(duration)
+
 
 print("Time to load images from memory, and prossess.")
 for i in range(100):
 
     print(f"{i}/100")
 
+    start = time.time()
+
     frame = get_frame(i)
+
     bgr = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
     results = hands.process(bgr)
 
-    hands_results.append(results)
+    end = time.time()
+    duration = end - start
 
-# dura.flag()
-#
-# print("Time to load images from cashe, draw results and save.")
-# for i in range(100):
-#
-#     print(f"{i}/100")
-#
-#     frame = get_frame_RGB(i)
-#
-#     path = f"data/video_Hol/frame_{i}.png"
-#
-#     cv2.imwrite(path, frame)
-#
-#     results
+    total_times.append(duration)
 
 
-dura.flag()
-dura.head()
+baseline_b = numpy.array(baseline_b)
+total_times = numpy.array(total_times)
 
+proc_times = total_times - baseline_b
 
-successes = [
-    results.multi_hand_landmarks is not None for results in hands_results
-]
-
-count = numpy.sum(successes)
-
-print(successes)
-print(count)
-
-
-
-# on bad data set 0%
-# static_image_mode=False)
-# EvaluateHands.py:42 -> EvaluateHands.py:52 = 7253.62ms.
-# EvaluateHands.py:52 -> EvaluateHands.py:82 = 12685.14ms.
-
-# 12685.14 - 7253.62 = 5431.52
-
-# 5431.52 per 100 images
-# 54.3152 per frame
-
-# 18.4110525231 FPS
-
-
-
-# static_image_mode=True)
-# EvaluateHands.py:42 -> EvaluateHands.py:52 = 7162.8ms.
-# EvaluateHands.py:52 -> EvaluateHands.py:82 = 12588.83ms.
-
-# 12588.83 - 7162.8 = 5426.03
-
-# 5426.03 per 100 images
-# 54.2603 per frame
-
-# 18.4296806321 FPS
+numpy.savetxt("1_HAN_C.txt", proc_times)
 
 
 
 
-# on bad data set 100%
-# static_image_mode=False)
 
-# EvaluateHands.py:42 -> EvaluateHands.py:52 = 7240.78ms.
-# EvaluateHands.py:52 -> EvaluateHands.py:82 = 15643.72ms.
+baseline_a = []
+baseline_b = []
+total_times = []
 
-# 15643.72 - 7240.78 = 8402.94
-
-# 8402.94 per 100 images
-# 84.0294 per frame
-
-# 11.9005966959 FPS
+mp_hands = mediapipe.solutions.hands
+hands = mp_hands.Hands(static_image_mode=True)
 
 
+print("Time to load images from memory and convert to RGB again")
+for i in range(100):
 
-# static_image_mode=True)
-# EvaluateHands.py:42 -> EvaluateHands.py:52 = 7121.58ms.
-# EvaluateHands.py:52 -> EvaluateHands.py:82 = 15205.59ms.
+    print(f"{i}/100")
 
-# 15205.59 - 7121.58 = 8084.01
+    start = time.time()
 
-# 8084.01 per 100 images
-# 80.8401 per frame
+    frame = get_frame(i)
 
-# 12.3700985031 FPS
+    bgr = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
+    end = time.time()
+    duration = end - start
+
+    baseline_a.append(duration)
+
+
+print("Time to load images from memory and convert to RGB again")
+for i in range(100):
+
+    print(f"{i}/100")
+
+    start = time.time()
+
+    frame = get_frame(i)
+
+    bgr = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    end = time.time()
+    duration = end - start
+
+    baseline_b.append(duration)
+
+
+print("Time to load images from memory, and prossess.")
+for i in range(100):
+
+    print(f"{i}/100")
+
+    start = time.time()
+
+    frame = get_frame(i)
+
+    bgr = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    results = hands.process(bgr)
+
+    end = time.time()
+    duration = end - start
+
+    total_times.append(duration)
+
+
+baseline_b = numpy.array(baseline_b)
+total_times = numpy.array(total_times)
+
+proc_times = total_times - baseline_b
+
+numpy.savetxt("1_HAN_S.txt", proc_times)
--- EvaluateZoom.py
+++ EvaluateZoom.py
@@ -1,19 +1,16 @@
-from functools import lru_cache
+import time
 import cv2
 
 import numpy
-from modules.duration import Duration
 from modules.eye import Eye
-from modules.video import Video
-from modules.zoomLive import Zoom
 
-vid = Video(canvas_framing=(1, 1))
-eye = Eye("left")
+from modules.zoomLive import Zoom
 
 hand_finder = Zoom(continuous=True)
 
-
-hands_list = []
+baseline_a = []
+baseline_b = []
+total_times = []
 
 
 def get_frame(i):
@@ -24,94 +21,135 @@ def get_frame(i):
     return frame
 
 
-dura = Duration()
+print("Time to load images from memory and convert to RGB again")
+for i in range(100):
 
-dura.head()
+    print(f"{i}/100")
+
+    start = time.time()
+
+    frame = get_frame(i)
+
+    frame_small = numpy.array(frame[::4, ::4], dtype=numpy.uint8)
+
+    bgr = cv2.cvtColor(frame_small, cv2.COLOR_BGR2RGB)
+
+    end = time.time()
+    duration = end - start
+
+    baseline_a.append(duration)
+
 
 print("Time to load images from memory and convert to RGB again")
 for i in range(100):
 
     print(f"{i}/100")
 
-    frame = get_frame(i)
-
-    frame_small = numpy.array(frame[::4, ::4], dtype=numpy.uint8)
-
-    bgr = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-dura.flag()
-print("Time to load images from memory and convert to RGB again")
-for i in range(100):
-
-    print(f"{i}/100")
+    start = time.time()
 
     frame = get_frame(i)
 
     frame_small = numpy.array(frame[::4, ::4], dtype=numpy.uint8)
 
-    bgr = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    bgr = cv2.cvtColor(frame_small, cv2.COLOR_BGR2RGB)
 
-dura.flag()
+    end = time.time()
+    duration = end - start
+
+    baseline_b.append(duration)
+
 
 print("Time to load images from memory, and prossess.")
 for i in range(100):
 
     print(f"{i}/100")
 
+    start = time.time()
+
     frame = get_frame(i)
 
     hand = hand_finder.get_hand(frame)
 
-    hands_list.append(hand)
+    end = time.time()
+    duration = end - start
 
-# dura.flag()
-# 
-# print("Time to load images from cashe, draw results and save.")
-# for i in range(100):
-# 
-#     print(f"{i}/100")
-# 
-#     frame = get_frame(i)
-# 
-#     hand = hands_list[i]
-# 
-#     frame = hand.draw(frame)
-# 
-#     path = f"data/video_Zoom/frame_{i}.png"
-# 
-#     cv2.imwrite(path, frame)
+    total_times.append(duration)
 
 
-dura.flag()
-dura.head()
+baseline_b = numpy.array(baseline_b)
+total_times = numpy.array(total_times)
+
+proc_times = total_times - baseline_b
+
+numpy.savetxt("1_DZ_C.txt", proc_times)
 
 
-successes = [hand.is_seen() for hand in hands_list]
-
-count = numpy.sum(successes)
-
-print(successes)
-print(count)
-
-# Static mode
-# 75%
-
-# EvaluateZoom.py:29 -> EvaluateZoom.py:42 = 7433.44ms.
-# EvaluateZoom.py:42 -> EvaluateZoom.py:54 = 7246.88ms.
-# EvaluateZoom.py:54 -> EvaluateZoom.py:85 = 16596.01ms.
 
 
-# 9349.13 for 100
-# 93.49 for 1
+
+baseline_a = []
+baseline_b = []
+total_times = []
+hand_finder = Zoom(continuous=False)
+
+print("Time to load images from memory and convert to RGB again")
+for i in range(100):
+
+    print(f"{i}/100")
+
+    start = time.time()
+
+    frame = get_frame(i)
+
+    frame_small = numpy.array(frame[::4, ::4], dtype=numpy.uint8)
+
+    bgr = cv2.cvtColor(frame_small, cv2.COLOR_BGR2RGB)
+
+    end = time.time()
+    duration = end - start
+
+    baseline_a.append(duration)
 
 
-# Continuous Mode
-# 76% 
+print("Time to load images from memory and convert to RGB again")
+for i in range(100):
 
-# EvaluateZoom.py:29 -> EvaluateZoom.py:42 = 7166.38ms.
-# EvaluateZoom.py:42 -> EvaluateZoom.py:54 = 7121.85ms.
-# EvaluateZoom.py:54 -> EvaluateZoom.py:85 = 12912.03ms.
+    print(f"{i}/100")
 
-# 5790.18 for 100
-# 57.9018 for 1
+    start = time.time()
 
+    frame = get_frame(i)
+
+    frame_small = numpy.array(frame[::4, ::4], dtype=numpy.uint8)
+
+    bgr = cv2.cvtColor(frame_small, cv2.COLOR_BGR2RGB)
+
+    end = time.time()
+    duration = end - start
+
+    baseline_b.append(duration)
+
+
+print("Time to load images from memory, and prossess.")
+for i in range(100):
+
+    print(f"{i}/100")
+
+    start = time.time()
+
+    frame = get_frame(i)
+
+    hand = hand_finder.get_hand(frame)
+
+    end = time.time()
+    duration = end - start
+
+    total_times.append(duration)
+
+
+baseline_b = numpy.array(baseline_b)
+total_times = numpy.array(total_times)
+
+proc_times = total_times - baseline_b
+
+numpy.savetxt("1_DZ_S.txt", proc_times)
